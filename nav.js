7a475c030e23ebc2096608ad3f7a64c7a42ba66b
as2xo <107620261+as2xo@users.noreply.github.com>
2023-03-08 00:18:41 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,2 +1,14 @@
 document.write('<div class="header"> <div class="sdvon"></div> <a href="/Kalyambikus/index.html" class="nav-li">Главная</a> <a href="/Kalyambikus/donate.html" class="nav-li">Донат</a><a href="/Kalyambikus/kolkoiny.html" class="nav-li">КольКоины</a><a href="/Kalyambikus/discord-rolle.html" class="nav-li">Роли в дискорд</a><a href="/Kalyambikus/contacts.html" class="nav-li">Контакты</a><a href="/Kalyambikus/all-roulette.html" class="nav-li-roulette">Рулетки</a></div>');
 
+  document.body.onclick = (event) => {
+    const elem = event.target;
+    // console.log(elem);
+    if (elem.classList.contains('section-symbol')) {
+        console.log(elem);
+        navigator.clipboard.writeText(elem.innerHTML)
+            .then(() => {
+                document.querySelector('').innerHTML += 'copy<br>';
+            })
+            alert('Роль скопирована. Однако, обратите внимание, уведомление не сработает простой вставкой в discord, так как имеет специальные символы.');
+    }
+}
