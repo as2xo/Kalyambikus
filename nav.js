8d9ecdced9040660b38f17a3db45222768dc810b
as2xo <107620261+as2xo@users.noreply.github.com>
2023-03-08 15:55:58 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,3 +1,16 @@
+document.write('<div id="page-preloader" class="preloader"><div class="loader"></div></div>');
+
+document.body.onload = function(){
+
+    setTimeout(function(){
+        const preloader = document.getElementById('page-preloader');
+        if (!preloader.classList.contains('done'))
+        {
+            preloader.classList.add('done');
+            preloader.style.zIndex = '-1';
+        }
+    }, 50)
+}
 document.write('<div class="header"> <div class="sdvon"></div> <a href="/Kalyambikus/index.html" class="nav-li">Главная</a> <a href="/Kalyambikus/donate.html" class="nav-li">Донат</a><a href="/Kalyambikus/kolkoiny.html" class="nav-li">КольКоины</a><a href="/Kalyambikus/discord-rolle.html" class="nav-li">Роли в дискорд</a><a href="/Kalyambikus/contacts.html" class="nav-li">Контакты</a><a href="/Kalyambikus/all-roulette.html" class="nav-li-roulette">Рулетки</a></div>');
 
   document.body.onclick = (event) => {
